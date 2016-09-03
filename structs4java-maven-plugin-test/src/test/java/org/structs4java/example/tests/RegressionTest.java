f68b649961254da02071fadc2d589a3ac1fd2524
Marc-Christian Schulze <Marc.Christian.Schulze@gmail.com>
2016-09-03 10:04:46 +0200

    Added more tests

--- structs4java-maven-plugin-test/src/test/java/org/structs4java/example/tests/RegressionTest.java
+++ structs4java-maven-plugin-test/src/test/java/org/structs4java/example/tests/RegressionTest.java
@@ -1,6 +1,7 @@
 package org.structs4java.example.tests;
 
 import java.io.IOException;
+import java.nio.ByteOrder;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -29,15 +30,65 @@ public class RegressionTest extends AbstractTest {
 
 		Assert.assertEquals(SimpleEnum.SecondItem, actual);
 	}
-	
+
 	@Test
 	public void testAdvancedStructure() throws IOException {
 		AdvancedStructure expected = createAdvancedStruct();
-		
+
 		expected.write(buffer);
 		buffer.position(0);
 
 		AdvancedStructure actual = AdvancedStructure.read(buffer);
 		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testHashCode() throws IOException {
+		AdvancedStructure a1 = createAdvancedStruct();
+		AdvancedStructure a2 = createAdvancedStruct();
+
+		Assert.assertEquals(a1.hashCode(), a2.hashCode());
+	}
+
+	@Test
+	public void testToString() throws IOException {
+		AdvancedStructure a1 = createAdvancedStruct();
+		AdvancedStructure a2 = createAdvancedStruct();
+
+		Assert.assertEquals(a1.toString(), a2.toString());
+	}
+
+	@Test
+	public void testBigEndian() throws IOException {
+		AdvancedStructure expected = createAdvancedStruct();
+
+		buffer.order(ByteOrder.BIG_ENDIAN);
+		expected.write(buffer);
+		buffer.position(0);
+
+		AdvancedStructure actual = AdvancedStructure.read(buffer);
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test
+	public void testLittleEndian() throws IOException {
+		AdvancedStructure expected = createAdvancedStruct();
+
+		buffer.order(ByteOrder.LITTLE_ENDIAN);
+		expected.write(buffer);
+		buffer.position(0);
+
+		AdvancedStructure actual = AdvancedStructure.read(buffer);
+		Assert.assertEquals(expected, actual);
+	}
+
+	@Test(expected = IOException.class)
+	public void testMixedEndian() throws IOException {
+		buffer.order(ByteOrder.LITTLE_ENDIAN);
+		SimpleEnum.SecondItem.write(buffer);
+		buffer.position(0);
+		buffer.order(ByteOrder.BIG_ENDIAN);
+
+		SimpleEnum.read(buffer);
 	}
 }
