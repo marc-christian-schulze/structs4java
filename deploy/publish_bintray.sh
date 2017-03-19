0ff4c58ccad7dfb7ef8221bd3a7505961266c288
Marc-Christian Schulze <Marc.Christian.Schulze@gmail.com>
2017-03-19 10:01:47 +0100

    Fixed profile activation

--- deploy/publish_bintray.sh
+++ deploy/publish_bintray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-    mvn verify --settings $GPG_DIR/settings.xml -Drelease-composite=true
+    mvn verify --settings $GPG_DIR/settings.xml -DperformUpdateSiteRelease=true
     exit $?
 fi
