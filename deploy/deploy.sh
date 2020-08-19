99eee0b39dc937c18a73b8ca0180a93ec573354e
Marc-Christian <Marc.Christian.Schulze@gmail.com>
2020-08-19 15:38:52 +0200

    Next try, cf. https://github.com/travis-ci/travis-ci/issues/5538

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+echo "Starting Deployment..."
+
 openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/pubring.gpg.enc -out $GPG_DIR/pubring.gpg -d
 openssl aes-256-cbc -pass pass:$ENCRYPTION_PASSWORD -in $GPG_DIR/secring.gpg.enc -out $GPG_DIR/secring.gpg -d
 
