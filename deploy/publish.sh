8ec5ff8990029f2b48b381ead47d97b742173cc9
Marc-Christian <Marc.Christian.Schulze@gmail.com>
2024-11-26 14:47:54 +0100

    Integrated artifact signing and removed TravisCI leftovers

--- deploy/publish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
-    export MAVEN_OPTS="--add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.text=ALL-UNNAMED --add-opens=java.desktop/java.awt.font=ALL-UNNAMED"
-    mvn deploy --settings $GPG_DIR/settings.xml -DperformRelease=true -DskipTests=true -Drelease-composite=true
-    exit $?
-fi
